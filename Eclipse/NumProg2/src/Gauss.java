4a50edc46e0a9cee58d902dafe0f3bce646834bc
Franz <fette_scheisse@web.de>
2014-05-30 01:18:09 +0100

    numProg2 finished my part

--- Eclipse/NumProg2/src/Gauss.java
+++ Eclipse/NumProg2/src/Gauss.java
@@ -44,35 +44,38 @@ public class Gauss {
 		// get size of vector for further calculation
 		int size = b.length-1;
 		// x vector
-		double[] x = new double[size+1];
 		double[][]a = A;	//working matrix
+		double[] d = b;		// working vector
 		
 		
-		for(int i = 0; i <= size ; i++){		
-				//sort
-				while(check == false){
-					check = true;
-					for(int j = i; 0 <= size; j++){
-						if (Math.abs(a[i][i]) <  Math.abs(a[j][i])){
-							tempArr = a[i];
-							a[i] = a[j];
-							a[j] = tempArr;
-							check = false;
-						}
+		for(int i = 0; i < size ; i++){		
+				//pivot search + switch rows
+			while(check == false){
+				check = true;
+				for(int j = i; j < size; j++){
+					if (Math.abs(a[i][i]) <  Math.abs(a[j][i])){
+						tempArr = a[i];
+						a[i] = a[j];
+						a[j] = tempArr;
+						temp = d[i];
+						d[i] = d[j];
+						d[j] = temp;
+						check = false;
 					}
 				}
-				//produce zeroes
-				for(int j = i; j <=size; j++){
-					temp = a[j+1][j]/a[j][j];		//calculate factor for the row
-					for( int k = i; k <= size; k++){
-						a[j+1][k]=a[i][j];
+			}
+			//produce zeroes
+			for(int j = i; j <size; j++){
+				for(int k = i+1; k <= size-j; k++){
+					temp = (a[j+k][j]/a[j][j]);		//calculate factor for the row
+					for( int l = i; l <= size; l++){
+						a[j+k][l]=a[j+k][l] - temp * a[j][l];	//matrix calculation
 					}
+					d[j+k]=d[j+k] - temp * d[j];		//vector calculation
 				}
-				
+			}
 		}
-		
-		
-		return x;
+		return backSubst(a, d);
 	}
 
 	/**
@@ -94,7 +97,65 @@ public class Gauss {
 	 */
 	public static double[] solveSing(double[][] A) {
 		//TODO: Diese Methode ist zu implementieren
-		return new double[2];
+		
+		// no testing here. just programmed in the dark.
+		// also beware dirty coding. just wanted to finish.
+		
+		double temp;
+		double[] tempArr;		//used for calculation
+		boolean check = false;
+		// get size of vector for further calculation
+		int size = A.length-1;
+		// x vector
+		double[][]a = A;	//working matrix
+		
+		//working vector filled with zeroes
+		double[] d = new double[A.length];
+		for	(int i = 0; i < d.length; i++){
+			d[i]= 0;
+		}
+		
+		
+		for(int i = 0; i < size ; i++){		
+				//pivot search + switch rows
+			while(check == false){
+				check = true;
+				for(int j = i; j < size; j++){
+					if (Math.abs(a[i][i]) <  Math.abs(a[j][i])){
+						tempArr = a[i];
+						a[i] = a[j];
+						a[j] = tempArr;
+						check = false;
+					}
+				}
+				// no pivot detected.
+				if (a[i][i] < 1E-10){
+					double[][] T = new double [i][i];
+					double[] v = new double [i];
+					for(int j = 0; j <=i; j++){
+						v[j] = -a[j][i];			//negation included
+						for(int k = 0; k <= i; k++){
+							T[j][k]=a[j][k];
+						}
+					}
+					tempArr = solve(T, v);
+					for(int j = 0 ; j <= i ; j++){
+						d[i] = tempArr[i];
+					}
+				}
+				
+			}
+			//produce zeroes
+			for(int j = i; j <size; j++){
+				for(int k = i+1; k <= size-j; k++){
+					temp = (a[j+k][j]/a[j][j]);		//calculate factor for the row
+					for( int l = i; l <= size; l++){
+						a[j+k][l]=a[j+k][l] - temp * a[j][l];	//matrix calculation
+					}
+				}
+			}
+		}
+		return d;
 	}
 
 	/**
--- Eclipse/NumProg2/src/myTest.java
+++ Eclipse/NumProg2/src/myTest.java
@@ -13,8 +13,31 @@ public class myTest {
 		
 		double[] x = Gauss.backSubst(R, b);
 		
+		System.out.println("backSubst()");
 		for(int i=0; i <b.length; i++){
 			System.out.println(x[i]);
+		}
+
+		
+		double [][] A = {{18, 4, 3},
+						 {4, 9, 4},
+						 {4, 7, 8}};
+		double [] d = {2, 5, 2};
+		
+		double[] y = Gauss.solve(A,d);
+		
+		System.out.println("solve()");
+		for(int i=0; i <d.length; i++){
+			System.out.println(y[i]);
+		}
+		
+		double [][] B = {{3, 5, 4},
+						 {6, 7, 8},
+						 {1, 2, 3}};
+		double[] z = Gauss.solveSing(B);
+		System.out.println("solveSing()");
+		for(int i=0; i <d.length; i++){
+			System.out.println(z[i]);
 		}
 	}
 }
